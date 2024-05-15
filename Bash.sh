780f99733d4c1c80b8abd3fe07b0b3eb79121409
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-05-15 09:59:06 +0300

    squash!

---
+++ Bash.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+git submodule add --depth 1 ssh://git@github.com/CodeEditorLand/SetupBiome.git
+git submodule add --depth 1 ssh://git@github.com/CodeEditorLand/Biome.git
+git submodule add --depth 1 ssh://git@github.com/CodeEditorLand/BiomeIntelliJ.git
+git submodule add --depth 1 ssh://git@github.com/CodeEditorLand/BiomeLand.git
+git submodule add --depth 1 ssh://git@github.com/CodeEditorLand/DiscordUtilsBot.git
+git submodule add --depth 1 ssh://git@github.com/CodeEditorLand/PreCommit.git
+git submodule add --depth 1 ssh://git@github.com/CodeEditorLand/Resources.git
